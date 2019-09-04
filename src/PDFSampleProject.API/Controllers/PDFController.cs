e697cfd09124d7475e32631460d789de24c2a9a2
david_sentinel <david@sentinel.no>
2019-09-04 08:03:51 +0200

    updated controller and launch settings

--- src/PDFSampleProject.API/Controllers/PDFController.cs
+++ src/PDFSampleProject.API/Controllers/PDFController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PDFSampleProject.API.Abstraction;
 
@@ -15,11 +16,34 @@ namespace PDFSampleProject.API.Controllers
             _pdfService = pdfService;
         }
 
-        [HttpGet("Create")]
-        public async Task<IActionResult> CreatePdf()
+        [HttpGet] // https://localhost:5001/api/pdf
+        public async Task<IActionResult> Get()
         {
             var file = await _pdfService.Create();
             return File(file, "application/pdf");
+        }
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+    }
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
         }
     }
 }
